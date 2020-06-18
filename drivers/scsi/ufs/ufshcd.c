462c0f92114ca5d4c253ef8a56f87fbcb611dfbf
Kiwoong Kim <kwmad.kim@samsung.com>
2020-06-18 15:35:55 +0900

    FROMLIST: ufs: introduce a callback to get info of command completion
    Some SoC specific might need command history for
    various reasons, such as stacking command contexts
    in system memory to check for debugging in the future
    or scaling some DVFS knobs to boost IO throughput.
    
    What you would do with the information could be
    variant per SoC vendor.
    
    Bug: 163100341
    Link: https://lore.kernel.org/linux-scsi/159530290480.22526.15318134249960762889.b4-ty@oracle.com/T/#t
    Change-Id: I39a30ff742d3a2eebb66decb651e4cf5b990c1eb
    Signed-off-by: Kiwoong Kim <kwmad.kim@samsung.com>
    Acked-by: Stanley Chu <stanley.chu@mediatek.com>
    Reviewed-by: Avri Altman <avri.altman@wdc.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -5109,6 +5109,7 @@ static void __ufshcd_transfer_req_compl(struct ufs_hba *hba,
 	for_each_set_bit(index, &completed_reqs, hba->nutrs) {
 		lrbp = &hba->lrb[index];
 		cmd = lrbp->cmd;
+		ufshcd_vops_compl_xfer_req(hba, index, (cmd) ? true : false);
 		if (cmd) {
 			trace_android_vh_ufs_compl_command(hba, lrbp);
 			ufshcd_add_command_trace(hba, index, "complete");
