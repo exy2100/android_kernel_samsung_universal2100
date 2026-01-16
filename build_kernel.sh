68655ed3b4df10034a33cd0528a12ea4c22204d1
Atakan <ata.kaner03@gmail.com>
2026-01-16 12:21:44 +0300

    universal2100: Import G998BXXUEHYD5 from OSRC
    Change-Id: Icd7a3e1b6d24644f096bbd70e784b0fce5bfbabd

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=15
 
-make ARCH=arm64 exynos2100-o1sxxx_defconfig
+make ARCH=arm64 exynos2100-p3sxxx_defconfig
 make ARCH=arm64 -j16
